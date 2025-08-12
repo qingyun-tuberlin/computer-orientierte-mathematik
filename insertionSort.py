211e3d61fe59297dfd4f9eb826b881db183e6a2f
qingyun-tuberlin <qingyuntub0@gmail.com>
2025-08-12 10:36:56 +0200

    Update insertionSort.py

--- insertionSort.py
+++ insertionSort.py
@@ -1,14 +1,11 @@
 def insertionSort(arr):
     
     printArray(arr)
-    print("#")
+    print("************")
     for i in range(1, len(arr)):
         s = arr[i]
         k = i 
-
-        # Move elements of arr[0..i-1], that are
-        # greater than key, to one position ahead
-        # of their current position
+        
         print("#")
         while k > 0 and s < arr[k-1]:
             arr[k] = arr[k-1]
