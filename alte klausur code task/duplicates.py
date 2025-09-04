55d137cfbde6269cbf4c40e120e6197976bd21a5
qingyun-tuberlin <qingyuntub0@gmail.com>
2025-09-04 21:30:31 +0200

    .

---
+++ alte klausur code task/duplicates.py
@@ -0,0 +1,7 @@
+# Task c (3 points)
+# Es sei eine Liste L von n ganzen Zahlen gegeben. Schreiben Sie eine Python-Funktion
+# duplicates (L), die in Laufzeit O(nlogn) eine geordnete Liste der Elemente aus L
+# zurückgibt, welche in L mindestens doppelt vorkommen.
+
+def duplicates(L):
+    pass
