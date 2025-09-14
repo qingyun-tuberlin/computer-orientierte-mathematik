650925c91992aee3dba5d3c6715b541e351a0a46
qingyun-tuberlin <qingyuntub0@gmail.com>
2025-09-14 17:56:47 +0200

    matroide exercises

---
+++ k8-9-sortierung/insertionSort.py
@@ -0,0 +1,24 @@
+def insertionSort(arr):
+    
+    printArray(arr)
+    print("************")
+    for i in range(1, len(arr)):
+        s = arr[i]
+        k = i 
+        
+        print("#")
+        while k > 0 and s < arr[k-1]:
+            arr[k] = arr[k-1]
+            k -= 1
+            printArray(arr)
+        arr[k] = s
+        printArray(arr)
+        
+# A utility function to print array of size n
+def printArray(arr):
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
+    print()
+
+arr = [5, 4, 3, 2, 1]
+insertionSort(arr)
